9574c728c1a0959c5288e77cf23f6cc27271bae8
mark.webb <mark@webbwebbwebb.co.uk>
2018-03-03 16:03:44 +0000

    Added Large Text File Viewer

---
+++ large-text-file-viewer/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'LTFViewr.zip'
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  file          = $fileLocation
+  checksum      = 'FAA740CD2214A1A166A612712D5D9BF97810A4157D2E006F80EA63BF48704C13'
+  checksumType  = 'sha256'
+}
+Get-ChocolateyUnzip $fileLocation $toolsDir
